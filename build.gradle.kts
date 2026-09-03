6da10d26d1391cd032676ab033e9bb6af2479a40
lbao39913-png <lbao39913@gmail.com>
2026-09-03 23:47:07 +0800

    chore: configure plugins in root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    // root-level build config if needed
+    id("com.android.application") version "8.6.1" apply false
+    id("com.android.library") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
 
 allprojects {
