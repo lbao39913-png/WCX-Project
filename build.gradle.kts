67adbbe19708ccfc6b5a448bd3f27b02cb6dd97a
lbao39913-png <lbao39913@gmail.com>
2026-09-03 23:42:55 +0800

    chore: add root build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    // root-level build config if needed
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
