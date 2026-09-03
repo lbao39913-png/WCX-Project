21d3d485de58041a811053f65811081a7f4d9237
lbao39913-png <lbao39913@gmail.com>
2026-09-03 23:49:38 +0800

    chore: add app build.gradle.kts (application module)

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.wcx.project"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.wcx.project"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":qrcode"))
+    implementation(project(":clonevoice"))
+    implementation(project(":fakelocation"))
+    implementation(project(":virtualvoipvideo"))
+    implementation(project(":hidecontacts"))
+    implementation(project(":license"))
+    implementation(project(":hook"))
+
+    implementation("androidx.core:core-ktx:1.11.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
