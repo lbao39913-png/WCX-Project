22a472b5efc32d9217aab7eff8a239685c855235
lbao39913-png <lbao39913@gmail.com>
2026-09-03 23:50:55 +0800

    chore: add core module build.gradle.kts

---
+++ core/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.wcx.project.core"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 35
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.11.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
+}
