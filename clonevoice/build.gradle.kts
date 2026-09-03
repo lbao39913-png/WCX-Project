d195e69cfe99ceb11cf522023ebe62d435a0acae
lbao39913-png <lbao39913@gmail.com>
2026-09-04 00:08:55 +0800

    fix: add clonevoice library build file (scaffold)

---
+++ clonevoice/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.wcx.project.clonevoice"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 35
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
+}
